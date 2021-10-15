d985bd1300122efd6904cdc0ac22ec8ee3249980
Ukasz <lukaszmarcinwojcik@gmail.com>
2021-10-15 04:42:28 +0200

    config db

---
+++ config.js
@@ -0,0 +1,7 @@
+// db satellite
+module.exports = {
+    keySession: ["kluczsesji"],
+    maxAgeSession: 24 * 60 * 60 * 1000,
+    db: "mongodb+srv://adminandrzej12345:fCDhhgnJ9FbW9L4@cluster0.1nwsn.mongodb.net/satellitesapp?retryWrites=true&w=majority",
+    JWT_SECRET: "sasasdasd34da",
+  };
